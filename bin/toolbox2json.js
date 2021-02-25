63a64a88d1afd5b77c69e710b8cc2c45e804b352
Daniel W. Hieber <dwhieb@gmail.com>
2021-02-24 20:14:02 -0600

    NEW: command line interface (#7)
    closes #7

---
+++ bin/toolbox2json.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import toolbox2json  from '../toolbox2json.js';
+
+toolbox2json();
--- toolbox2json.js
+++ toolbox2json.js
@@ -1,3 +1,12 @@
+import createSpinner from 'ora';
+
 export default function toolbox2json() {
+
+  const spinner = createSpinner(`Converting Toolbox file.`);
+
+  spinner.start();
+  spinner.succeed(`Toolbox file converted.`);
+
   return ``;
+
 }
