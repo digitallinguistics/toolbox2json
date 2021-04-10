4df77c9e7bb3174092a34b77b039af9abc21ec24
Daniel W. Hieber <dwhieb@gmail.com>
2021-04-09 21:01:05 -0500

    update command line script

--- bin/toolbox2json.js
+++ bin/toolbox2json.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
-import { Command }           from 'commander/esm.mjs';
-import { load as parseYAML } from 'js-yaml';
-import path                  from 'path';
-import { readFileSync }      from 'fs';
-import toolbox2json          from '../src/toolbox2json.js';
+import { Command }       from 'commander/esm.mjs';
+import { fileURLToPath } from 'url';
+import path              from 'path';
+import { stringify }     from 'ndjson';
+import toolbox2json      from '../src/toolbox2json.js';
 
-import { fileURLToPath, pathToFileURL } from 'url';
+import {
+  createWriteStream,
+  readFileSync,
+} from 'fs';
 
 const currentDir = path.dirname(fileURLToPath(import.meta.url));
 
@@ -23,69 +26,22 @@ program
 .description(`parse the Toolbox file`, {
   filePath: `path to the Toolbox file`,
 })
-.option(`-m, --mappings <mappingsPath>`, `path to file specifying line marker > property name mappings`)
 .option(`-n, --ndjson`, `output newline-delimited JSON`, false)
 .option(`-o, --out <outPath>`, `path for the output JSON file`)
-.option(`-p, --postprocessor <postprocessorPath>`, `path to the postprocessor file`)
-.option(`-s, --silent`, `silences console output`, false)
-.option(`-t, --transforms <transformsPath>`, `path to transformations file`)
-.action(async (filePath, options) => {
+.action((filePath, options) => {
 
-  const {
-    mappings:      mappingsPath,
-    out:           outPath,
-    postprocessor: postprocessorPath,
-    transforms:    transformsPath,
-  } = options;
+  const entries = toolbox2json(filePath, options);
 
-  // get mappings from file
+  if (!options.out) return console.info(entries);
 
-  let mappings = {};
+  const jsonStream  = stringify();
+  const writeStream = createWriteStream(options.out);
 
-  if (mappingsPath) {
-
-    const mappingsExt  = path.extname(mappingsPath);
-    const mappingsText = readFileSync(mappingsPath, `utf8`);
-
-    if (mappingsExt === `.json`) {
-      mappings = JSON.parse(mappingsText);
-    } else {
-      mappings = parseYAML(mappingsText);
-    }
-
+  for (const entry of entries) {
+    jsonStream.write(entry);
   }
 
-  // get postprocessor function from file
-
-  let postprocessor = entry => entry;
-
-  if (postprocessorPath) {
-    const importPath = pathToFileURL(path.join(process.cwd(), postprocessorPath));
-    ({ default: postprocessor } = await import(importPath));
-  }
-
-  // get transforms from file
-
-  let transforms = {};
-
-  if (transformsPath) {
-    const importPath         = pathToFileURL(path.join(process.cwd(), transformsPath));
-    ({ default: transforms } = await import(importPath));
-  }
-
-  // run module
-
-  Object.assign(options, {
-    mappings,
-    postprocessor,
-    transforms,
-  });
-
-  const readStream = toolbox2json(filePath, options);
-
-  if (!outPath) {
-    readStream.on(`data`, console.info);
-  }
+  jsonStream.pipe(writeStream);
 
 });
 
